e321ea09ca64c84494da79068b07b0bc1b1088d1
hyewon <auddnjswls@naver.com>
2023-03-16 09:24:11 +0900

    study

---
+++ index.py
@@ -0,0 +1,17 @@
+import sayHello
+
+print(sayHello.hello('superman'))
+
+print("d안녕ㅇㅇㅇㅇㅇㅇㅇㅇ")
+
+print( "qksrkrkrkrkr")
+
+print("44444444")
+
+print( "555555555")
+
+print("6666666666")
+
+print("집에서해보는중")
+
+print("학원에서 다시 보내는중")
